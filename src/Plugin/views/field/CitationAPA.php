4ba0a047c94aa9f5640e3d91605225d8b03be997
Bethany <bethany@seeger.ws>
2021-05-25 17:51:36 +0000

    Adds a service and a few fields to help a view grab citations.

---
+++ src/Plugin/views/field/CitationAPA.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\idc_export\Plugin\views\field\CitationAPA
+ */
+
+namespace Drupal\idc_export\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Seboettg\CiteProc\StyleSheet;
+use Seboettg\CiteProc\CiteProc;
+
+/**
+ * Field handler to get the local_id for a node
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("citation_apa")
+ */
+class CitationAPA extends CitationField {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    \Drupal::logger('idc_export')->info('rendering the node citation_apa field');
+    \Drupal::logger('idc_export')->info('originalObject: ' . $values->_originalObject->title[0]);
+    \Drupal::logger('idc_export')->info('title via index: ' . $values->_item->getField('title')->getValues()[0]);
+    \Drupal::logger('idc_export')->info('title via index: ' . $values->_item->getField('field_description')->getValues()[0]);
+
+    $metadata = $this->formatMetadata($values);
+    $style = 'apa';
+    $citation = \Drupal::service('citations.default')->renderFromMetadata($metadata, $style, 'bibliography');
+ 
+    \Drupal::logger('idc_export')->info('citation is ' . $citation);
+    return $this->t($citation);
+  }
+}
---
+++ src/Plugin/views/field/CitationMLA.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\idc_export\Plugin\views\field\CitationMLA
+ */
+
+namespace Drupal\idc_export\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Seboettg\CiteProc\StyleSheet;
+use Seboettg\CiteProc\CiteProc;
+
+/**
+ * Field handler to get the local_id for a node
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("citation_mla")
+ */
+class CitationMLA extends CitationField {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    \Drupal::logger('idc_export')->info('rendering the node citation_mla field');
+
+    //FIX THIS FIX THIS -- print_r not working
+    //\Drupal::logger('idc_export')->info('what\'s avail: ' . print_r($values->_entity, true));
+    
+    //\Drupal::logger('idc_export')->info('title is: ' . $values->title[0]['raw']['value']);
+    //\Drupal::logger('idc_export')->info('description is: ' . $values->field_field_description[0]['raw']['value']);
+    //\Drupal::logger('idc_export')->info('item id: ' . $values->_itemId);
+    //\Drupal::logger('idc_export')->info('originalObject: ' . $values->_originalObject->title[0]);
+    \Drupal::logger('idc_export')->info('title via index: ' . $values->_item->getField('title')->getValues()[0]);
+    \Drupal::logger('idc_export')->info('title via index: ' . $values->_item->getField('field_description')->getValues()[0]);
+    //\Drupal::logger('idc_export')->info('title via index: ' . $values->_item->getField('description'));
+    //dpm($values);
+    //dpm($values->_item->getField('title')->getValues()[0]);
+
+    // create json file of information
+    //$data = file_get_contents("metadata.json");
+    //
+    //
+    $metadata = $this->formatMetadata($values);
+    $style = "modern-language-association";
+    //$style = "ieee";
+    //dpm($metadata);
+    $citation = \Drupal::service('citations.default')->renderFromMetadata($metadata, $style, 'bibliography');
+ 
+    \Drupal::logger('idc_export')->info('citation is ' . $citation);
+    return $this->t($citation);
+
+   /*
+    $node = $values->_entity;
+    if ($node->bundle() == $this->options['node_type']) {
+      return $this->t('Hey, I\'m of the type: @type', array('@type' => $this->options['node_type']));
+    }
+    else {
+      return $this->t('Hey, I\'m something else.');
+    }
+    */
+  }
+}
